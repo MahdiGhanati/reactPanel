09e728419c7a3facecefdaa9a432d221b7179763
unknown <mghanaty@gmail.com>
2024-03-20 11:37:31 +0330

    update add and deploy

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World 0
+          Hello World
         </p>
       </header>
     </div>
